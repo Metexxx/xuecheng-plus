a3affc8d64448a85e89d284762f3e4224450bfb4
霜降 <12525692+shuangjiang_me@user.noreply.gitee.com>
2025-01-29 21:08:03 +0800

    create parent and base module.

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/XueChengPlusBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueChengPlusBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueChengPlusBaseApplication.class, args);
+    }
+
+}
