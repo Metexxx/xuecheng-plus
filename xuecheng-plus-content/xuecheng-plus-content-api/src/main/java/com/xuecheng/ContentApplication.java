f33468c8d23e3b100bad19664bcf85895d783f09
霜降 <12525692+shuangjiang_me@user.noreply.gitee.com>
2025-01-31 17:22:58 +0800

    first api test.

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
@@ -2,11 +2,11 @@ package com.xuecheng;
 
 
 import com.spring4all.swagger.EnableSwagger2Doc;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+@EnableSwagger2Doc
 @SpringBootApplication
 public class ContentApplication {
     public static void main(String[] args) {
--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/CourseBaseInfoController.java
@@ -1,4 +1,4 @@
-package com.xuecheng.controller;
+package com.xuecheng.content;
 
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
@@ -6,9 +6,14 @@ import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @description 课程信息编辑接口
@@ -18,12 +23,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(value = "课程信息编辑接口", tags = "课程信息编辑接口")
 @RestController
+@RequestMapping("/course")
 public class CourseBaseInfoController {
     @ApiOperation("课程查询接口")
-    @RequestMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParams){
-
-        return null;
+    @PostMapping("/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParams){
+        CourseBase courseBase = new CourseBase();
+        courseBase.setName("测试名称");
+        courseBase.setCreateDate(LocalDateTime.now());
+        List<CourseBase> courseBaseList = new ArrayList<>();
+        courseBaseList.add(courseBase);
+        return new PageResult<>(courseBaseList, 10, 1, 10);
     }
 }
 
