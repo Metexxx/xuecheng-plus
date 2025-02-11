c1ecf33b62a720495634e21ec2bb9b0d16a23db9
霜降 <12525692+shuangjiang_me@user.noreply.gitee.com>
2025-02-11 21:07:18 +0800

    课程计划排序、删除

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -4,9 +4,11 @@ import com.xuecheng.content.model.dto.TeachplanDto;
 import com.xuecheng.content.model.po.Teachplan;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -19,4 +21,8 @@ import java.util.List;
 public interface TeachplanMapper extends BaseMapper<Teachplan> {
 
     List<TeachplanDto> selectTreeNodes(long courseId);
+
+    List<TeachplanDto> selectChildNodes(Long id);
+
+    void updateBatchById(@Param("updateList") List<Teachplan> updateList);
 }
